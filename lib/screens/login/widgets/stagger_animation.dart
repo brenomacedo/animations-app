22a0f100d84abb4354686c4e3df84ea260b70cab
brenosekiro <b88660501@gmail.com>
2021-03-06 15:45:20 -0300

    animatingButton

--- lib/screens/login/widgets/stagger_animation.dart
+++ lib/screens/login/widgets/stagger_animation.dart
@@ -1,30 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
 
 class StaggerAnimation extends StatelessWidget {
 
   final AnimationController controller;
 
-  StaggerAnimation({ this.controller });
+  StaggerAnimation({ this.controller }) :
+    buttonSqueeze = Tween(begin: 320.0, end: 60.0).animate(CurvedAnimation(parent: controller,
+      curve: Interval(0.0, 0.15)));
+
+  final Animation<double> buttonSqueeze;
+
+  Widget _buildInside(BuildContext context) {
+    if(buttonSqueeze.value > 75) {
+      return Text('Sign in', style: TextStyle(
+        color: Colors.white,
+        fontSize: 20,
+        fontWeight: FontWeight.w300,
+        letterSpacing: 0.3
+      ));
+    } else {
+      return CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.white), strokeWidth: 1.0);
+    }
+  }
 
   Widget _buildAnimation(BuildContext context, Widget child) {
     return Padding(
       padding: EdgeInsets.only(bottom: 50),
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          controller.forward();
+        },
         child: Container(
-          width: 320,
+          width: buttonSqueeze.value,
           height: 60,
           alignment: Alignment.center,
           decoration: BoxDecoration(
             color: Color.fromRGBO(247, 64, 106, 1.0),
             borderRadius: BorderRadius.all(Radius.circular(30.0))
           ),
-          child: Text('Sign in', style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.w300,
-            letterSpacing: 0.3
-          )),
+          child: _buildInside(context),
         ),
       ),
     );
