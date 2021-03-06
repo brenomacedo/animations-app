b1079acfcdfbc58e3cc2d1205b34f7f5d051d08f
brenosekiro <b88660501@gmail.com>
2021-03-06 17:26:37 -0300

    bar

--- lib/screens/home/widgets/category_view.dart
+++ lib/screens/home/widgets/category_view.dart
@@ -6,6 +6,23 @@ class CategoryView extends StatefulWidget {
 }
 
 class _CategoryViewState extends State<CategoryView> {
+
+  final List<String> categories = ['Trabalho', 'Estudos', 'Casa'];
+
+  int _category = 0;
+
+  void selectForward() {
+    setState(() {
+      _category++;      
+    });
+  }
+
+  void selectBackward() {
+    setState(() {
+       _category--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -13,14 +30,17 @@ class _CategoryViewState extends State<CategoryView> {
       children: [
         IconButton(
           icon: Icon(Icons.arrow_back_ios, color: Colors.white),
-          onPressed: () {},
+          onPressed: _category > 0 ? selectBackward : null,
+          disabledColor: Colors.white30,
         ),
         Text(
-          "ESTUDAR", style: TextStyle()
+          categories[_category].toUpperCase(), style: TextStyle(fontSize: 18, letterSpacing: 1.2,
+            fontWeight: FontWeight.w300, color: Colors.white)
         ),
         IconButton(
           icon: Icon(Icons.arrow_forward_ios, color: Colors.white),
-          onPressed: () {},
+          onPressed: _category < categories.length - 1 ? selectForward : null,
+          disabledColor: Colors.white30,
         )
       ],
     );
